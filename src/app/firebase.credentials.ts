fa865df8cf96b77a65872ee9b2297325d8e846d8
Himynameispaul <pauli.j.jokiranta@student.laurea.fi>
2018-11-18 23:49:25 +0200

    Initial Commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+    
+    apiKey: "AIzaSyBio5ocAdvAfi5QXQLXW2O1EC5BNHAOuwE",
+    authDomain: "songlyrics-bbca3.firebaseapp.com",
+    databaseURL: "https://songlyrics-bbca3.firebaseio.com",
+    projectId: "songlyrics-bbca3",
+    storageBucket: "songlyrics-bbca3.appspot.com",
+    messagingSenderId: "146516002081"
+  
+
+}
